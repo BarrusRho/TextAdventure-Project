49a75de85f7cc386d74f1cea8652e61fcf7e22c8
BarrusRho <60799948+BarrusRho@users.noreply.github.com>
2022-03-12 10:51:04 +0200

    Process user input

--- TextAdventure/Assets/Scripts/GameController.cs
+++ TextAdventure/Assets/Scripts/GameController.cs
@@ -28,5 +28,32 @@ namespace TextAdventure
             description += $"{player.currentLocation.GetConnectionsText()}";
             currentText.text = $"{description}";
         }
+
+        public void TextEntered()
+        {
+            Debug.Log($"Text entered");
+            LogCurrentText();
+            ProcessInput(textEntryField.text);
+            textEntryField.text = $"";
+            textEntryField.ActivateInputField();
+
+        }
+
+        public void LogCurrentText()
+        {
+            historyText.text += $"\n\n";
+            historyText.text += $"{currentText.text}";
+            historyText.text += $"\n\n";
+            historyText.text += $"{textEntryField.text}";
+        }
+
+        public void ProcessInput(string input)
+        {
+            input = input.ToLower();
+            char[] delimiter = {' '};
+            string[] separatedWords = input.Split(delimiter);
+
+            currentText.text = $"Nothing happens! Having trouble? Type Help";
+        }
     }
 }
